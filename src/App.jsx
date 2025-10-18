c1cecaf6356fa78229c9d7d9681c778de505272b
Amirsina A. Albekord <aalbekor@uci.edu>
2025-10-18 13:17:01 -0700

    added and tested api call

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
-
+import Gallery from "./components/Gallery"
+import BanList from "./components/BanList"
 function App() {
   const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
   const [currentImage, setCurrentImage] = useState(null);
@@ -10,18 +11,43 @@ function App() {
     "classification": "Painting"
   });
   const makeQuery = () => {
-    let query = ``
+    let randomPage = Math.floor(Math.random() * 100)
+    let query = `https://api.harvardartmuseums.org/image?q=width:>2000&apikey=${ACCESS_KEY}&page=${randomPage}`
     callAPI(query).catch(console.error);
   }
   const callAPI = async (query) => {
     const response = await fetch(query);
     const json = await response.json();
     console.log(json)
+    if(json == null) {
+      alert("Something went wrong with querying!")
+    }
+    else {
+      let randomImage = Math.floor(Math.random() * json.info.totalrecordsperquery)
+      setCurrentImage(json.records[randomImage].baseimageurl)
+      setPrevImages((images) => [...images, json.records[randomImage].baseimageurl])
+    }
   }
-  makeQuery()
+  
   return (
     <>
-    <a href="https://github.com/harvardartmuseums/api-docs/blob/master/README.md">Harvard Art Museums</a>
+    <div className="whole-page">
+      <Gallery />
+      <div className="container">
+        <a href="https://github.com/harvardartmuseums/api-docs/blob/master/README.md">Harvard Art Museums</a>
+        {currentImage ? (
+          <img
+            className="art-image"
+            src={currentImage}
+            alt="Harvard Art Museum piece"
+          />
+        ) : (
+          <div> </div>
+        )}
+        <button className="query-button" onClick={makeQuery} />
+      </div>
+      <BanList />
+    </div>
     </>
   )
 }
